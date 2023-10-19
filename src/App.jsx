49210595aec994e8d3bffb20791de5b7b3f6315d
babywipez <cassada.chris@gmail.com>
2023-10-19 09:08:24 -0400

    add Bio component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Bio from "./components/Bio";
+
 const App = () => {
-
   return (
-    <>
+    <div className="bg-gray-950">
       <h1 className="bg-slate-600">webworks</h1>
-    </>
-  )
-}
+      <Bio />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "libre-basker": ['"Libre Baskerville"'],
+        "libre-franklin": ['"Libre Franklin"'],
+      },
+      colors: {
+        "warhouse-red": "#f21326",
+      },
+      listStyleImage: {
+        square: 'url("/rectangle.svg")',
+      },
+    },
   },
   plugins: [],
-}
-
+};
