1f1df7fb93e31d3ec7baa2a821d84516ce353a95
Wyatt Rajer <wyattrajer@gmail.com>
2023-10-19 12:27:07 -0400

    added deployment packages and scripts

--- src/components/NavModal.jsx
+++ src/components/NavModal.jsx
@@ -1,28 +1,26 @@
 const NavModal = () => {
   return (
-    <>
-      <section className="text-gray-50">
-        <ul className="flex flex-row mx-10 list-outside items-center">
-          <a className="mx-2 hover:marker:text-warhouse-red" href="#about">
+    <section className="flex flex-col items-center font-libre-basker">
+      <section className="flex flex-col w-10/12 text-gray-50">
+        <ul className="flex flex-col w-full  my-10 text-3xl list-inside">
+          <a className="mx-2 my-6 hover:marker:text-warhouse-red border-b-2 py-1 border-b-gray-50" href="#about">
             <li>About</li>
           </a>
-          <a className="mx-2 ml-10 hover:marker:text-warhouse-red" href="#location">
+          <a className="mx-2 my-6 hover:marker:text-warhouse-red border-b-2 py-1 border-b-gray-50" href="#location">
             <li>Gear</li>
           </a>
           <a
-            className="mx-2 ml-10 hover:marker:text-warhouse-red"
+            className="mx-2 my-6 hover:marker:text-warhouse-red border-b-2 py-1 border-b-gray-50"
             href="#portfolio"
           >
             <li>Clients</li>
           </a>
         </ul>
-      </section>
-      <section className="flex text-gray-50">
-        <button className="hover:text-warhouse-red hover:border-warhouse-red border-2 border-gray-50 rounded-sm p-2 px-6 mx-2">
+        <button className="hover:text-warhouse-red hover:border-warhouse-red border-2 border-gray-50 text-gray-50 rounded-sm p-2">
           Contact
         </button>
       </section>
-    </>
+    </section>
   );
 };
 
