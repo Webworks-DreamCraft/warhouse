9b240086248e2f5927b76da8e2841877a9eb2e0b
Christopher Cassada <39451144+babywipez@users.noreply.github.com>
2023-10-19 19:37:58 -0400

    feat(Bio) - Add component & styles (#3)
    * add Bio component and styles
    
    * adjust flex styles for img in Bio
    
    * adjust responsive layout for project
    
    * remove br elements & include implicit return

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-const App = () => {
+import Bio from "./components/Bio";
 
+const App = () => {
   return (
     <>
       <h1 className="bg-slate-600">webworks</h1>
+      <div className="bg-gray-950">
+        <div className="container mx-auto">
+          <Bio />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
