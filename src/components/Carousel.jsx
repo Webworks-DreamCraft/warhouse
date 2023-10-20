708c4b976465bf5c718fb14e01ac4e6685195937
babywipez <cassada.chris@gmail.com>
2023-10-19 21:29:51 -0400

    add custom media query for ultra small viewport

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,9 +2,18 @@ import Target from "../../public/target2.svg";
 
 const Carousel = () => {
   return (
-    <div>
-      <h1 className="text-gray-50">Carousel Goes Here</h1>
-    </div>
+    <section className="text-gray-50 text-center p-5 sm:text-left lg:pt-10 lg:pb-10 lg:pr-10 lg:pl-10">
+      <h2 className="relative px-7 pt-5 mb-5 inline-block font-libre-franklin text-2xl font-bold xxs:text-4xl sm:text-7xl sm:pl-0 sm:pr-14 sm:pt-12">
+        <img
+          src={Target}
+          className="absolute top-0 right-0 max-w-[45px] sm:max-w-[90px]"
+        />
+        RECORDINGS
+      </h2>
+      <p className="text-center mb-8 text-lg font-libre-basker sm:text-left sm:text-sm sm:mb-4">
+        Recorded, Mixed, or Mastered
+      </p>
+    </section>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xxs: "350px",
+      ...defaultTheme.screens,
+    },
     extend: {
       fontFamily: {
         "libre-basker": ['"Libre Baskerville"'],
